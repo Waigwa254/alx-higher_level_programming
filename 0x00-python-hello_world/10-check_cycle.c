fadd047f6e19f59999c9e68912b6e7afd58e1a14
Waigwa254 <augustinewaigwa254@gmail.com>
2023-11-28 10:53:10 -0800

    10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * check_cycle - contain cycles
+ * @list: list
+ * Return: 1
+ */
+int check_cycle(listint_t *list)
+{
+listint_t *slow = list;
+listint_t *fast = list;
+
+if (!list)
+return (0);
+while (slow && fast && fast ->next)
+{
+slow = slow ->next;
+fast = fast->next->next;
+if (slow == fast)
+return (1);
+}
+return (0);
+}
