efc364201915d789548b6da9950ac7e9e1dfca77
Waigwa254 <augustinewaigwa254@gmail.com>
2023-11-29 11:27:28 -0800

    alpha

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for alpha in range(97, 123):
-    if alpha != 101 and alpha != 113:
-        print("{:c}".format(alpha), end="")
-
+for alpa in range(97, 123):
+    if alpa != 101 and alpa != 113:
+        print("{:c}".format(alpa), end="")
