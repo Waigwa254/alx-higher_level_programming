ddfbe29ea6aa55eb73cdc402ada79a520b8472b8
Waigwa254 <augustinewaigwa254@gmail.com>
2023-11-30 11:50:31 -0800

    my calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,38 +1,26 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
-
-def my_calculator():
-    if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    a, operator, b = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    if operator not in ('+', '-', '*', '/'):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    try:
-        a, b = int(a), int(b)
-    except ValueError:
-        print("Invalid input. Please provide valid integers for <a> and <b>")
-        sys.exit(1)
-
-    result = 0
-
-    if operator == '+':
-        result = add(a, b)
-    elif operator == '-':
-        result = sub(a, b)
-    elif operator == '*':
-        result = mul(a, b)
-    elif operator == '/':
-        result = div(a, b)
-
-    print(f"{a} {operator} {b} = {result}")
-    sys.exit(0)
-
 if __name__ == "__main__":
-    my_calculator()
+    import sys
+    from calculator_1 import add, sub, mul, div
+    argv = sys.argv[1:]
+    argv_count = len(argv)
+    operators = ["+", "-", "*", "/"]
+    if argv_count != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif sys.argv[2] not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if sys.argv[2] == "+":
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        elif sys.argv[2] == "-":
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        elif sys.argv[2] == "*":
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        elif sys.argv[2] == "/":
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+
 
