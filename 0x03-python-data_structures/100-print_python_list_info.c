8ae4fd7e4157a02e9b484918934ea3b2f79f79c2
Waigwa254 <augustinewaigwa254@gmail.com>
2023-12-04 08:44:28 -0800

    adv

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,16 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <python.h>
-
+#include <Python.h>
 /**
- * print_python_list_info -print python lists
- * @p: python 
+ * print_python_list_info -  functis
+ * @p: py
  */
-void print_python_list_info(PyObject*p)
+void print_python_list_info(PyObject *p)
 {
 	int elem;
-	printf("[*]Size of the Python List = %lu\n", Py_SIZE(p));
-	printf("[*]Allocated = %lu\n",((PyListObject*)p)->allocated):
+
+	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
 	for (elem = 0; elem < Py_SIZE(p); elem++)
-		printf("Element %d: %s\n", elem, Py_TYPE(PyList_Getltem(p, elem))->tp_name);
+		printf("Element %d: %s\n", elem, Py_TYPE(PyList_GetItem(p, elem))->tp_name);
 }
+
