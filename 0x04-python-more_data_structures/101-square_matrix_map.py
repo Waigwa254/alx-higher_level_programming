72376ba566d44f0b9653b6dedb9a251ee1e0d97c
Waigwa254 <augustinewaigwa254@gmail.com>
2023-12-06 06:40:39 -0800

    adv

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    return list (map(lambda row: list(map(lambda col: col**2, row)), matrix))
+def square_matrix_map(matrix=[]):i
+    return list(map(lambda row: list(map(lambda col: col**2, row)), matrix))
