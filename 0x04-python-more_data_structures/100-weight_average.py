099070554c6a2980e7eadbd42355e8c40034ca93
Waigwa254 <augustinewaigwa254@gmail.com>
2023-12-06 06:43:48 -0800

    adv

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-def weigth_average(my_list=[]):
+def weight_average(my_list=[]):
     if not my_list:
         return 0
+
     num = 0
     den = 0
+
     for tup in my_list:
         num += tup[0] * tup[1]
         den += tup[1]
+
     return (num / den)
